bb3f26134f19b43167da97db5529eaf791f02873
9thCircle <the9thcircleofhelll@gmail.com>
2023-08-09 08:46:58 +0300

    Add project files.

---
+++ SleepTimer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TimedSleep;
+
+public static partial class Program
+{
+    [LibraryImport("Powrprof.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SetSuspendState([MarshalAs(UnmanagedType.Bool)] bool hibernate, [MarshalAs(UnmanagedType.Bool)] bool forceCritical, [MarshalAs(UnmanagedType.Bool)] bool disableWakeEvent);
+
+    private static void Main(string[] arg)
+    {
+        if (arg[0] == "a")
+        {
+            Process[] targetProcesses = Process.GetProcessesByName("TimedSleep");
+            foreach (var process in targetProcesses)
+                process.Kill();
+            return;
+        }
+
+        Thread.Sleep(int.Parse(arg[0]));
+
+        SetSuspendState(false, true, false); ;
+    }
+}
---
+++ SleepTimerStarter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ShutdownTimer;
+
+public static partial class Program
+{
+    private static Regex regex = TimeInput();
+
+    private static void Main()
+    {
+        int seconds = 0;
+
+        while (seconds < 1)
+        {
+            Console.WriteLine("Please provide number followed by one of these [s,m,h,d,y].");
+
+            string toParse = Console.ReadLine();
+
+            if (toParse == "a")
+            {
+                Process.Start(Path.Combine(Directory.GetCurrentDirectory(), @"SleepTimer.exe"), "a");
+                return;
+            }
+
+            Match match = regex.Match(toParse);
+
+            if (!match.Success)
+            {
+                Console.WriteLine("Input string was not in a correct format.");
+                Main();
+                return;
+            }
+
+            seconds = int.Parse(match.Groups["count"].Value) * (match.Groups["type"].Value switch
+            {
+                "s" => 1,
+                "m" => 60,
+                "h" => 3600,
+                "d" => 86400,
+                "y" => 31536000,
+                _ => 1
+            }) * 1000;
+        }
+
+        Process.Start(Path.Combine(Directory.GetCurrentDirectory(), @"SleepTimer.exe"), seconds.ToString());
+    }
+
+    private static string GetParentDirectory(string currentDir, int depth)
+    {
+        return depth == 0 ? currentDir : GetParentDirectory(Directory.GetParent(currentDir).FullName, depth - 1);
+    }
+
+
+    [GeneratedRegex(@"^(?<count>\d+)(?<type>[smhdy])$")]
+    private static partial Regex TimeInput();
+}
